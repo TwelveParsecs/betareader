ded15b6e4b03dc7db5c3545ec8e632fbdef004e7
David Brennan <davidmbrennan@outlook.com>
2017-02-21 09:47:35 -0500

    Started adding form variables

--- app/routes/newproject.js
+++ app/routes/newproject.js
@@ -4,8 +4,20 @@ export default Ember.Route.extend({
   actions: {
 
       createProject() {
+
          const controller = this.get("controller");
+         const userID = this.session
+         const manuscriptPath = ''//controller.get('manuscriptPath');
+         const previewPath = ''//controller.get('previewPath');
          const title = controller.get('title');
+         const description = controller.get('description');
+         const critiqueInstructions = controller.get('critiqueInstructions');
+         const wordsInPreview = controller.get('wordsInPreview');
+         const wordsInManuscript = controller.get('wordsInManuscript');
+         const ageLimit = controller.get('wordsInPreview');
+         const lookingFor = controller.get('lookingFor');
+         const tags = controller.get('tags');
+         const matureContent = controller.get('matureContent');
 
         // Create new project
         var project = this.store.createRecord('project', {
