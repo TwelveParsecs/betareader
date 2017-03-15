ba602cce3931935e4ea10fc2ee5e30ccfb34b054
David Brennan <davidmbrennan@outlook.com>
2017-03-15 16:59:04 -0400

    Add simple matching

--- app/routes/matches.js
+++ app/routes/matches.js
@@ -1,61 +1,73 @@
 import Ember from 'ember';
 import { storageFor } from 'ember-local-storage';
 
+let projects = [];
+
 export default Ember.Route.extend({
   sessionData: storageFor('session-data'),
-
   model(){
-    let projects = [];
+
     let writeTags = [];
     let user = this.get("sessionData.userID");
     let name = this.get("sessionData.name");
     let genresRead = this.get("sessionData.reads");
     let _this = this;
-    console.log(user);
+    let promise;
+    //console.log(user);
 
     // Get tags
     // Remove leading and trailing spaces and then split by comma delimiters
     var readTags = genresRead.split(',');
-    console.log(readTags.length);
+    //console.log(readTags.length);
 
-    // Find tags that match what the reader reads
-    for (let i = 0; i < readTags.length; i++){
+    // Wrap it all in a promise
+    return promise = new Ember.RSVP.Promise(function(resolve, reject) {
+      // Find tags that match what the reader reads
+      readTags.forEach(function(tag){
+          console.log(tag);
+           _this.store.query('tags', {
+            orderBy:"name",
+            equalTo: tag
+          }).then(function(results, index, enumerable){
 
-      console.log(readTags[i]);
-        _this.store.query('tags', {
-          orderBy:"name",
-          equalTo: readTags[i]
-        }).then(function(results){
-          results.forEach(function(result){
-            console.log("called 2");
-              // Add each tag result to array if that project id does not already exist
-              if (writeTags.includes(result.get("projectID")) === false){
-                
+              results.forEach(function(result){
+                  // Add each tag result to array if that project id does not already exist
 
-                writeTags.push(result.get("projectID"));
-                console.log("id: " + result.get("projectID") + ", tag: " + result.get("name"));
-              }
+                  if (writeTags.includes(result.get("projectID")) === false){
+                    writeTags.push(result.get("projectID"));
+
+                    // Query for the project that corresponds to that ID
+                    _this.store.query('project', {
+                      equalTo: result.get("projectID")
+                    }).then(function(results){
+
+                       results.forEach(function(result){
+
+                          projects.push({
+                            id: result.get("id"),
+                            title: result.get("title"),
+                            author: "Name",
+                            description: result.get("description"),
+                            tags:readTags
+                          });
+                      });
+
+                      // Resolve promise with array of projects
+                      resolve(projects);
+                      //console.log("id: " + result.get("projectID") + ", tag: " + result.get("name"));
+                    });
+                  }
+              });
           });
-      });
-    }
 
-    // return this.store.query('project', {
-    //   orderBy: 'userID', equalTo: user
-    // }).then(function(results){
-    //   results.forEach(function(result){
-    //       // Add results to an array with user data
-    //       console.log(result.get('id'));
-    //
-    //       projects.push({
-    //         id: result.get("id"),
-    //         title: result.get("title"),
-    //         author: name,
-    //         description: result.get("description"),
-    //         tags:result.get("tags")
-    //       });
-    //   });
-    //
-    //   return projects;
-    // });
+        });
+      }).then(function(results){
+        return results;
+      });
+
+
+
+    //console.log(projects);
+    //return projects;
   }
 });
