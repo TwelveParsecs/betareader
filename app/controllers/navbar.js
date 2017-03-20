7e3843052805e20ca26e81b508239010715f913c
David Brennan <davidmbrennan@outlook.com>
2017-03-19 20:41:33 -0400

    nav first pass style

--- app/controllers/navbar.js
+++ app/controllers/navbar.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  init: function(){
+    var route = this.get('routing').get('currentRouteName');
+    console.log(route);
+  },
   actions: {
        logout: function() {
            this.get('session').close().then(function() {
