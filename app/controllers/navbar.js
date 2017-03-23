5468a869451dfeb98246f957bf7fa804cc725dd4
David Brennan <davidmbrennan@outlook.com>
2017-03-23 00:30:02 -0400

    nav item highlighting kind of working

--- app/controllers/navbar.js
+++ app/controllers/navbar.js
@@ -1,15 +1,44 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  matches: false,
+  newproject: false,
+  routeName: " ",
   init: function(){
-    var route = this.get('routing').get('currentRouteName');
-    console.log(route);
+    // Get current route
+    this._super(...arguments);
+    let appCtrl = Ember.getOwner(this).lookup('controller:application');
+    this.set ("routeName",appCtrl.currentRouteName);
+
+    switch(this.get("routeName")){
+      case "matches":
+        this.set("matches",true);
+        this.set("newproject",false);
+        break;
+      case "newproject":
+        this.set("matches",true);
+        this.set("newproject",false);
+        break;
+    }
+
   },
   actions: {
        logout: function() {
            this.get('session').close().then(function() {
                this.transitionTo('login');
            }.bind(this));
+       },
+       changeUnderline: function(){
+         switch(this.get("routeName")){
+           case "matches":
+             this.set("matches",true);
+             this.set("newproject",false);
+             break;
+           case "newproject":
+             this.set("matches",true);
+             this.set("newproject",false);
+             break;
+         }
        }
    }
 });
