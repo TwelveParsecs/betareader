0673cb8c8b2e4e3b1295e9b7411e061eac1b0671
David Brennan <davidmbrennan@outlook.com>
2017-03-26 14:33:27 -0400

    full read more functionality

--- app/components/match-card.js
+++ app/components/match-card.js
@@ -5,8 +5,19 @@ export default Ember.Component.extend({
   actions:{
     toggleMore: function(event){
       let elements = event.element;
-      let full = Ember.$(elements).find(".full-description");
-      full.toggle(".hidden");
+      Ember.$(elements).find(".full-description").toggle(".hidden");
+      Ember.$(elements).find(".instructions-title").toggle(".hidden");
+      Ember.$(elements).find(".instructions").toggle(".hidden");
+      Ember.$(elements).find(".hide-gradient").toggle(".hidden");
+
+      if (this.get("readMore")){
+        Ember.$(elements).find(".read-more").html("Read More&hellip;");
+        this.set("readMore",false);
+      }
+      else{
+        Ember.$(elements).find(".read-more").html("Read Less&hellip;");
+        this.set("readMore",true);
+      }
     }
   }
 });
