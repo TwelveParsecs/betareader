91083318a6c77ca9369d6f6d6391dd40112bdad2
David Brennan <davidmbrennan@outlook.com>
2017-04-15 17:04:32 -0400

    finish responsive css critiques

--- tests/unit/controllers/critiques-test.js
+++ tests/unit/controllers/critiques-test.js
@@ -1,11 +1,12 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:critique', 'Unit | Route | critique', {
+moduleFor('controller:critiques', 'Unit | Controller | critiques', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
+// Replace this with your real tests.
 test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+  let controller = this.subject();
+  assert.ok(controller);
 });
