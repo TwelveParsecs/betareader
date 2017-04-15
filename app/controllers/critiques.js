7c2370fe158675264416e9ffe289cf11a2a7f0c2
David Brennan <davidmbrennan@outlook.com>
2017-04-15 17:24:58 -0400

    current and past critique toggle

--- app/controllers/critiques.js
+++ app/controllers/critiques.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  current:"true",
+  actions: {
+    toggle(current){
+      this.set("current",current);
+
+      if (current == "true"){
+        Ember.$("#current-link").addClass("selected");
+        Ember.$("#past-link").removeClass("selected");
+
+        Ember.$("#current").removeClass("hidden");
+        Ember.$("#past").addClass("hidden");
+      }
+      else{
+        Ember.$("#current-link").removeClass("selected");
+        Ember.$("#past-link").addClass("selected");
+
+        Ember.$("#current").addClass("hidden");
+        Ember.$("#past").removeClass("hidden");
+      }
+    }
+  }
 });
